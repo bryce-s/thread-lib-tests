62d86df3a85d3504ce17bbe0cd2d5b0d7f245c6a
bryce-s <bryces@umich.edu>
2019-02-16 17:43:22 -0500

    added test loop

--- run_tests.py
+++ run_tests.py
@@ -6,10 +6,13 @@ from sty import fg, bg, ef, rs, RgbFg
 
 
 def compileProject():
-    # search our makefile for compile targets:
+    # search our makefile for compile targets. Return execution targets.
+    executionTargets = list()
     makeTargets = list()
     with open("Makefile", "r") as makefile:
-        queryRes = re.findall(r'\w+:\s', makefile.read())
+        mf_str = makefile.read()
+        executionTargets = re.findall(r'-o\s\w+', mf_str)
+        queryRes = re.findall(r'\w+:\s', mf_str)
         normalized = list()
         for res in queryRes:
             normalized.append(re.search(r'\w+', res)[0])
@@ -21,13 +24,41 @@ def compileProject():
     for target in makeTargets:
        completeProcess = subprocess.run("make " + target, capture_output=True, text=True, shell=True)
        if completeProcess.returncode != 0:
-           print(fg.red + "make failed in: " + target)
-           print(str(completeProcess.stderr) + fg.rs)
-           
+            print(fg.red + "make failed in: " + target)
+            print(str(completeProcess.stderr) + fg.rs)
+            exit(1o1:)
+       else:
+            print(fg.green + target + " built!" + fg.rs)
+
+    return executionTargets
 
 
-def runTest():
-    pass
+def runTest(execTarget: str):
+    """returns 0 if test ran sucessfully, 1 if skip, else >1"""
+    runString = "./" + execTarget
+    print("Calling: " + runString)
+    correctOutput = "tests/output/" + execTarget + "_out_correct"
+    if os.path.isfile(correctOutput):
+        completeProcess = subprocess.run(runTest, capture_output=True, text=True, shell=True)
+        with open("tests/output/" + execTarget + "_out_correct") as correctFile:
+            correctText = correctFile.read()
+            if correctText != completeProcess.stdout:
+                print(fg.red + execTarget + " differs from correct file!" + fg.rs)
+                return 2
+            else:
+                return 0
+    else:
+        print(fg.yellow + "No matching correct file found for " + execTarget + ", skipping" + fg.rs)
+        return 1
+
+def runAllTests(executionTargets: list):
+    for target in executionTargets:
+        for i in range(0, 10):
+            res = runTest(target)
+            if res == 1 or res > 1:
+                break
+
+
 
 def main():
     """entry point for test program"""
